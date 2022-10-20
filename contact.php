0af67ad0c576688d748e24213dcfa58e4b125b25
nehashaikh913 <nehashaikh0500@gmail.com>
2022-10-20 13:21:47 +0530

    contact faq changes

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
         </div>
         <!-- End Page Title Area -->
         <!-- Start Contact Us Area -->
-        <div class="contact-area pt-100">
+        <div class="contact-area pt-100 pb-5">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 col-md-12">
@@ -74,16 +74,11 @@
                             <h3>Contact Information</h3>
                             <ul>
                                 <li><span>Hotline:</span> <a href="tel:12855">12855</a></li>
-                                <li><span>Tech support:</span> <a href="tel:+1514312-5678">+1 (514) 312-5678</a></li>
                                 <li><span>Email:</span> <a href="https://templates.envytheme.com/cdn-cgi/l/email-protection#6d05080101022d1d0c190204430e0200"><span class="__cf_email__" data-cfemail="3c54595050537c4c5d485355125f5351">[email&#160;protected]</span></a></li>
                                 <li><span>Address:</span> 1523 Cook Hill Road New Haven, CT</li>
-                                <li><span>Available:</span> Monday - Friday 8:00am - 8:00pm</li>
                             </ul>
                         </div>
                     </div>
-                </div>
-                <div id="maps">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46660.669043361966!2d-76.17429939609431!3d43.03529129888566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d9f3b8019f2991%3A0x58d5929e21a62e5!2sDinosaur%20Bar-B-Que!5e0!3m2!1sen!2sbd!4v1593527523138!5m2!1sen!2sbd"></iframe>
                 </div>
             </div>
         </div>
